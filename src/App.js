6b37105b73a385e717747bc286e016683801dc8e
Antonija <antonija.kasum@gmail.com>
2018-11-04 17:07:05 +0200

    Bugs fixed and some refactoring

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import EntryContainer from './components/EntryContainer';
 
-function putInLocalStorage(arr, local) {
-  let bla = JSON.stringify(arr);
-  localStorage.setItem(local, bla);
-}
-
-function getFromLocalStorage(local) {
-  let result = JSON.parse(localStorage.getItem(local));
-  return result
-}
+// ********************************* HELPER FUNCTIONS *********************************
 
 function numbersWithZero(number) {
   return String("000" + number).slice(-2);
@@ -36,135 +28,192 @@ function userIdGenerator() {
   }
   return id
 }
+// ********************************* HANDLING DATA IN LOCAL STORAGE *********************************
+
+function putInLocalStorage(arraytoPut, lsArrayNameStr) {
+  localStorage.setItem(lsArrayNameStr, JSON.stringify(arraytoPut));
+}
+
+function getFromLocalStorage(lsArrayNameStr) {
+  return JSON.parse(localStorage.getItem(lsArrayNameStr));
+}
+
+let lsIncome = getFromLocalStorage('income')
+let lsExpense = getFromLocalStorage('expense')
+let lsTotalIncome = 0;
+let lsTotalExpense = 0;
+
+const getInitialTotals = () => {
+  lsIncome.forEach(income => lsTotalIncome += income.amount)
+  lsExpense.forEach(expense => lsTotalExpense += expense.amount)
+}
+
+getInitialTotals()
 
 // **************************************************************************************
-// **************************************************************************************
+// **********************************--- APP ---*****************************************
 // **************************************************************************************
 
 class App extends Component {
   state = ({
-    income: [{description: 'salary', amount: 300.51, time:displayDateTime(), id: userIdGenerator()}, {description: 'bonus', amount: 300.51, time:displayDateTime(), id: userIdGenerator()}],
-    expense:[{description: 'expense', amount: 300.51, time:displayDateTime(), id: userIdGenerator()}],
-    description: '',
-    amount: 0,
-    totalIncome: 601.02,
-    totalExpense: 300.51,
-    incomeOrExpense: 'income',
-    balance: 300.51,
-    setClassDescr: 'noWarning',
-    setClassAmnt: 'noWarning',
-    descrInputClass: 'default',
-    amntInputClass: 'default'
+    income: lsIncome, // gets initial value from local storage
+    expense: lsExpense, // gets initial value from local storage
+    totalIncome: lsTotalIncome, // gets initial value from local storage
+    totalExpense: lsTotalExpense, // gets initial value from local storage
+    balance: lsTotalIncome - lsTotalExpense,
+    description: '', // user input
+    amount: '', // user input
+    field: 'income', // selection 'income' or 'expense'
+    classNameDescrSpan: 'invisible', // opposite is 'visible'
+    classNameAmntSpan: 'invisible', // opposite is 'visible'
+    classNameDescrInput: 'default', // opposite is 'red'
+    classNameAmntInput: 'default' // opposite is 'red'
   })
-  setDescription = (e) => {
+  
+  getDescription = (e) => {
       this.setState({
         description: e.target.value.toString(), 
-        setClassDescr: 'noWarning', 
-        descrInputClass: 'default'
+        classNameDescrSpan: 'invisible', 
+        classNameDescrInput: 'default'
       })
   }
-  setAmount = (e) => {
-    let input = e.target.value
-    if(isNaN(input)){
+
+  getAmount= (e) => {
+    if(isNaN(e.target.value)){
       this.setState({
-        amount: 0, 
-        setClassAmnt: 'warning-visible',
-        amntInputClass: 'red'
+        amount: '', 
+        classNameAmntSpan: 'visible',
+        classNameAmntInput: 'red'
       })
     } else {
       this.setState({
-        amount: parseFloat(input), 
-        setClassAmnt: 'noWarning',
-        amntInputClass: 'default'
+        amount: parseFloat(e.target.value), 
+        classNameAmntSpan: 'invisible',
+        classNameAmntInput: 'default'
       })
     }
   }
-  setOption = (e) => {
-    this.setState({incomeOrExpense: e.target.value})
+
+  getOption = (e) => {
+    this.setState({field: e.target.value})
   }
+
   addEntry = () => {
-    if(this.state.incomeOrExpense === 'income' && this.state.amount !== '' && this.state.description !== ''){
-      const newId = userIdGenerator();
-      this.setState(
-        {income: 
-          [...this.state.income, 
-            {description: this.state.description,
-              amount: this.state.amount, 
-            time:displayDateTime(),
-            id: newId
-          }],
-          amount: '',
-          amField: '',
-          description:'',
-          totalIncome: this.state.totalIncome + this.state.amount, 
-          balance: this.state.balance + this.state.amount
-        });
-      putInLocalStorage(this.state.income, 'income')
-      this.descRef.value = ''
-      this.amtRef.value = ''
-    } else if (this.state.incomeOrExpense === 'expense' && this.state.amount !== '' && this.state.description !== '') {
-      this.setState({expense: 
-        [...this.state.expense, 
-          {description: this.state.description, 
-            amount: this.state.amount, 
+    if(this.state.field === 'income' && this.state.amount !== '' && this.state.description !== ''){
+
+      let newIncome = [
+        ...this.state.income, {
+          description: this.state.description,
+          amount: this.state.amount, 
           time:displayDateTime(),
           id: userIdGenerator()
-        }],
+        }
+      ]
+
+      this.setState({
+        income: newIncome,
+        amount: '',
+        amField: '',
+        description:'',
+        totalIncome: this.state.totalIncome + this.state.amount, 
+        balance: this.state.balance + this.state.amount
+      });
+
+      putInLocalStorage( newIncome, 'income');
+      this.descRef.value = '';
+      this.amtRef.value = '';
+
+    } else if (this.state.field === 'expense' && this.state.amount !== '' && this.state.description !== '') {
+
+      let newExpense = [
+        ...this.state.expense, {
+          description: this.state.description, 
+          amount: this.state.amount, 
+          time:displayDateTime(),
+          id: userIdGenerator()
+        }
+      ]
+
+      this.setState({
+        expense: newExpense,
         amount: '',
         amField: '',
         description:'',
         totalExpense: this.state.totalExpense + this.state.amount,
-        balance: this.state.balance - this.state.amount});
-      putInLocalStorage(this.state.expense, 'expense')
-      this.descRef.value = ''
-      this.amtRef.value = ''
-    } else if(this.state.amount === '' && this.state.description !== ''){
+        balance: this.state.balance - this.state.amount
+      });
+
+      putInLocalStorage(newExpense, 'expense');
+      this.descRef.value = '';
+      this.amtRef.value = '';
+
+    } else if (this.state.amount === '' && this.state.description !== '') {
+
       this.setState({
-        setClassAmnt: 'warning-visible',
-        amntInputClass: 'red'
+        classNameAmntSpan: 'visible',
+        classNameAmntInput: 'red'
       }); 
 
-    } else if(this.state.amount !== '' && this.state.description === ''){
+    } else if (this.state.amount !== '' && this.state.description === '') {
+
       this.setState({
-        setClassDescr: 'warning-visible',
-        descrInputClass: 'red'}); 
+        classNameDescrSpan: 'visible',
+        classNameDescrInput: 'red'
+      }); 
 
     } else {
+
       this.setState({
-        setClassDescr: 'warning-visible', 
-        setClassAmnt: 'warning-visible', 
-        amntInputClass: 'red', 
-        descrInputClass: 'red'}); 
+        classNameDescrSpan: 'visible', 
+        classNameDescrInput: 'red',
+        classNameAmntSpan: 'visible', 
+        classNameAmntInput: 'red' 
+      }); 
     }
   }
-  deleteEntryincome = (id) => {
-    if(window.confirm('Are you sure you want to delete this entry?')){
-    let deletedIncome;
-    let newIncome = this.state.income.filter(income => {
-        if(!income.id.includes(id)){
-          return income
-        } else {
-          deletedIncome = income.amount
-        }
-    })
-    this.setState({income: newIncome, totalIncome: this.state.totalIncome - deletedIncome, balance: this.state.totalIncome - deletedIncome - this.state.totalExpense})
+
+  deleteEntryIncome = (id) => {
+    if (window.confirm('Are you sure you want to delete this entry?')) {
+      let deletedIncome;
+
+      let newIncome = this.state.income.filter(income => {
+          if (!income.id.includes(id)) {
+            return income
+          } else {
+            deletedIncome = income.amount
+          }
+      })
+
+      this.setState({
+        income: newIncome, 
+        totalIncome: this.state.totalIncome - deletedIncome, 
+        balance: this.state.totalIncome - this.state.totalExpense - deletedIncome 
+      })
     }
   }
-  deleteEntryexpense = (id) => {
-    if(window.confirm('Are you sure you want to delete this entry?')){
-    let deletedExpense;
-    let newExpense = this.state.expense.filter(expense => {
-      let result;
-        if(!expense.id.includes(id)){
-          result = expense
-        } else {
-          deletedExpense = expense.amount
-        }
-        return result
-    })
-    this.setState({expense: newExpense, totalExpense: this.state.totalExpense - deletedExpense, balance: this.state.totalIncome - this.state.totalExpense + deletedExpense})
+
+  deleteEntryExpense = (id) => {
+    if (window.confirm('Are you sure you want to delete this entry?')) {
+      let deletedExpense;
+
+      let newExpense = this.state.expense.filter(expense => {
+        let result;
+
+          if (!expense.id.includes(id)) {
+            result = expense
+          } else {
+            deletedExpense = expense.amount
+          }
+          return result
+      })
+      this.setState({
+        expense: newExpense, 
+        totalExpense: this.state.totalExpense - deletedExpense, 
+        balance: this.state.totalIncome - this.state.totalExpense + deletedExpense
+      })
     }
   }
+
   makeBalanceRed = () => {
     if(this.state.balance < 0) {
       return "red-balance"
@@ -172,59 +221,72 @@ class App extends Component {
       return "default-balance"
     }
   }
+
   addMinus= () => {
-    if (this.state.totalExpense> 0){
+    if (this.state.totalExpense > 0) {
       return '-' + this.state.totalExpense.toFixed(2)
     } else {
       return this.state.totalExpense.toFixed(2)
     }
   }
+
   render() {
     return (
       <div className="App">
+
         <Header />
+
         <div id="input-container" >
-          <span  className={this.state.setClassDescr}>Please enter description</span>
-          <span  className={this.state.setClassAmnt}>Please enter the right amount using numbers</span>
+          <span  className={this.state.classNameDescrSpan}>Please enter description</span>
+          <span  className={this.state.classNameAmntSpan}>Please enter the right amount using numbers</span>
+          
           <input 
             type="text" 
             placeholder="Description" 
             id="description"  
-            className={this.state.descrInputClass} 
-            onInput={(e)=>{this.setDescription(e)}} 
+            className={this.state.classNameDescrInput} 
+            onInput={(e)=>{this.getDescription(e)}} 
             ref={(input)=>{this.descRef = input}}
           />
+
           <input 
             type="text" 
             placeholder="Amount" 
             id="amount" 
-            className={this.state.amntInputClass}
-            onInput={(e)=>{this.setAmount(e)}}
+            className={this.state.classNameAmntInput}
+            onInput={(e)=>{this.getAmount(e)}}
             ref={(input)=>{this.amtRef = input}}
           />
-          <select id="transaction-type" onChange={this.setOption}>
+
+          <select id="transaction-type" onChange={this.getOption}>
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
+
           <button id="add" onClick={this.addEntry}>Add</button>
+          
         </div>
-        <div id="main">
+
+        <main className="main">
+
           <EntryContainer 
           name='Income'
           id='income'
           total='Total income:'
           totalAmount={this.state.totalIncome.toFixed(2)}
           data={this.state.income}
-          deleteFunc={this.deleteEntryincome}
+          deleteFunc={this.deleteEntryIncome}
           />
+
           <EntryContainer 
           name='Expense'
           id='expense'
           total='Total expense:'
           totalAmount={this.addMinus()}
           data={this.state.expense}
-          deleteFunc={this.deleteEntryexpense}
+          deleteFunc={this.deleteEntryExpense}
           />
+
           <div id="balance-container">
             <h3 id="h3-balance">Balance</h3>
             <div className="total-container">
@@ -232,7 +294,9 @@ class App extends Component {
                 <span id="total-balance" className={this.makeBalanceRed()}>{this.state.balance.toFixed(2)}€</span>
             </div>
           </div>
-        </div>
+
+        </main>
+
       </div>
     );
   }
