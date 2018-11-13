126fa5ab7834db0bfc11974773977e625363fd94
Antonija <antonija.kasum@gmail.com>
2018-11-13 23:28:29 +0200

    Styling

--- src/components/EntryContainer.js
+++ src/components/EntryContainer.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import DeleteButton from './DeleteButton';
+import React from "react";
+import DeleteButton from "./DeleteButton";
 
-const EntryContainer = (props) => {
+const EntryContainer = props => {
   return (
-    <div className="entry-container" id={props.id + '-container'}>
+    <div className="entry-container" id={props.id + "-container"}>
+      <h3 id={"h3-" + props.id}>{props.name}</h3>
 
-      <h3 id={'h3-'+ props.id}>{props.name}</h3>
-
-        <div id={props.id}> 
-
-          {props.data.map(element => 
-            <div key={element.id + 'entry'} className="added">
-                <span className="dateAndTime">{element.time}</span>
-                <span id="description-value">{element.description}</span>
-                <span id="amount-value">
-                  {(props.name === 'Expense'? '-': '') + props.beautifyNumber(element.amount.toFixed(2))} €
-                </span>
-                <DeleteButton value={element.id} deleteFunc={props.deleteFunc}/>
-            </div>)
-          }
-
-        </div>
-
-      <div className="total-container" id={props.idForTotalContainer}>
-          <span>{props.total}</span>
-          <span id="total">
-            {(props.name === 'Expense'? '-': '') + props.beautifyNumber(props.totalAmount)} €
-          </span>
+      <div id={props.id}>
+        {props.data.map(element => (
+          <div key={element.id + "entry"} className="added">
+            <DeleteButton value={element.id} deleteFunc={props.deleteFunc} />
+            <span className="dateAndTime">{element.time}</span>
+            <span id="description-value">{element.description}</span>
+            <span id="amount-value">
+              {(props.name === "Expense" ? "-" : "") +
+                props.beautifyNumber(element.amount.toFixed(2))}{" "}
+              €
+            </span>
+          </div>
+        ))}
       </div>
 
+      <div className="total-container" id={props.idForTotalContainer}>
+        <span>{props.total}</span>
+        <span id="total">
+          {(props.name === "Expense" ? "-" : "") +
+            props.beautifyNumber(props.totalAmount)}{" "}
+          €
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default EntryContainer;
